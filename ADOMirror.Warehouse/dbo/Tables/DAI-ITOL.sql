b145288d3bf3ba07ac1ddc6c3486f5a23d5d3870
Reed Robison <26799308+breakpoint7@users.noreply.github.com>
2025-07-15 12:59:17 -0400

    Committing 6 items from workspace 36a59297-f3e9-471c-b564-ac15bf815938

---
+++ ADOMirror.Warehouse/dbo/Tables/DAI-ITOL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[DAI-ITOL] (
+
+	[WorkItemId] int NULL, 
+	[AnalyticsUpdatedDate] datetime2(6) NULL, 
+	[Title] varchar(8000) NULL, 
+	[WorkItemType] varchar(8000) NULL, 
+	[CreatedDate] datetime2(6) NULL, 
+	[State] varchar(8000) NULL, 
+	[ClosedDate] datetime2(6) NULL
+);
---
+++ ADOMirror.Warehouse/dbo/Tables/ProjectTracking.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[ProjectTracking] (
+
+	[ProjectName] varchar(8000) NULL, 
+	[ProjectSK] varchar(8000) NULL, 
+	[LastUpdated] datetime2(6) NULL
+);
